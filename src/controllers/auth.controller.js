529a824c5d6ce9f0ac7763d086b1487fb6d12be8
fede.zuccala <fede.zuccala22@gmail.com>
2022-01-09 19:00:27 -0300

    add refresh token and modify middleware auth

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,44 +1,59 @@
-'use strict'
+"use strict";
 
-const User = require('../models/user.model')
-const jwt = require('jsonwebtoken')
-const config = require('../config')
-const httpStatus = require('http-status')
-const uuidv1 = require('uuid/v1')
-
+const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+const httpStatus = require("http-status");
+const uuidv1 = require("uuid/v1");
+const { getEmailFromToken } = require("../utils/utilsFns");
 exports.register = async (req, res, next) => {
   try {
-    const activationKey = uuidv1()
-    const body = req.body
-    body.activationKey = activationKey
-    const user = new User(body)
-    const savedUser = await user.save()
-    res.status(httpStatus.CREATED)
-    res.send(savedUser.transform())
+    const activationKey = uuidv1();
+    const body = req.body;
+    body.activationKey = activationKey;
+    const user = new User(body);
+    const savedUser = await user.save();
+    res.status(httpStatus.CREATED);
+    res.send(savedUser.transform());
   } catch (error) {
-    return next(User.checkDuplicateEmailError(error))
+    return next(User.checkDuplicateEmailError(error));
   }
-}
+};
 
 exports.login = async (req, res, next) => {
   try {
-    const user = await User.findAndGenerateToken(req.body)
-    const payload = {sub: user.id}
-    const token = jwt.sign(payload, config.secret)
-    return res.json({ message: 'OK', token: token })
+    const user = await User.findAndGenerateToken(req.body);
+    const payload = { sub: user.id, email: user.email };
+    const token = jwt.sign(payload, config.secret, { expiresIn: "24h" });
+    return res.json({ message: "OK", token: token });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
+const handleRefreshToken = (user) => {
+  const payload = { sub: user.id, email: user.email };
+  const token = jwt.sign(payload, config.secret, { expiresIn: "24h" });
+  return { message: "OK", token: token };
+};
+exports.refreshToken = async (req, res, next) => {
+  try {
+    const email = getEmailFromToken(req);
+    const user = await User.findOne({ email: email });
+    const response = handleRefreshToken(user);
+    return res.json(response);
+  } catch (error) {
+    next(error);
+  }
+};
 
 exports.confirm = async (req, res, next) => {
   try {
     await User.findOneAndUpdate(
-      { 'activationKey': req.query.key },
-      { 'active': true }
-    )
-    return res.json({ message: 'OK' })
+      { activationKey: req.query.key },
+      { active: true }
+    );
+    return res.json({ message: "OK" });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
--- src/routes/api/auth.route.js
+++ src/routes/api/auth.route.js
@@ -1,28 +1,29 @@
-'use strict'
+"use strict";
 
-const express = require('express')
-const router = express.Router()
-const authController = require('../../controllers/auth.controller')
-const validator = require('express-validation')
-const { create } = require('../../validations/user.validation')
-const auth = require('../../middlewares/authorization')
+const express = require("express");
+const router = express.Router();
+const authController = require("../../controllers/auth.controller");
+const validator = require("express-validation");
+const { create } = require("../../validations/user.validation");
+const auth = require("../../middlewares/authorization");
 
-router.post('/register', validator(create), authController.register) // validate and register
-router.post('/login', authController.login) // login
-router.get('/confirm', authController.confirm)
+router.post("/register", validator(create), authController.register); // validate and register
+router.post("/login", authController.login); // login
+router.post("/refresh-token", authController.refreshToken); // refresh token
+router.get("/confirm", authController.confirm);
 
 // Authentication example
-router.get('/secret1', auth(), (req, res) => {
+router.get("/secret1", auth(), (req, res) => {
   // example route for auth
-  res.json({ message: 'Anyone can access(only authorized)' })
-})
-router.get('/secret2', auth(['admin']), (req, res) => {
+  res.json({ message: "Anyone can access(only authorized)" });
+});
+router.get("/secret2", auth(["admin"]), (req, res) => {
   // example route for auth
-  res.json({ message: 'Only admin can access' })
-})
-router.get('/secret3', auth(['user']), (req, res) => {
+  res.json({ message: "Only admin can access" });
+});
+router.get("/secret3", auth(["user"]), (req, res) => {
   // example route for auth
-  res.json({ message: 'Only user can access' })
-})
+  res.json({ message: "Only user can access" });
+});
 
-module.exports = router
+module.exports = router;
